fb71a027a1dcad360b5dcd626cd00809dad66238
Kathleen McLaughlin <kathleen.mclaughlin.17@gmail.com>
2017-05-26 10:36:02 -0400

    its done

--- ExamScenarios/ExamScenarios.playground/Pages/Scenario 3.xcplaygroundpage/Contents.swift
+++ ExamScenarios/ExamScenarios.playground/Pages/Scenario 3.xcplaygroundpage/Contents.swift
@@ -22,8 +22,38 @@ import PlaygroundSupport
 //: ## Your code starts here
 // Create a new canvas
 let canvas = Canvas(width: 400, height: 600)
+canvas.drawShapesWithBorders = false
 
+canvas.fillColor = Color(hue: 16, saturation: 83, brightness: 100, alpha: 100)
+canvas.drawRectangle(centreX: 200, centreY: 300, width: 400, height: 600)
 
+canvas.fillColor = Color(hue: 78, saturation: 6, brightness: 85, alpha: 100)
+var points:[NSPoint] = []
+points.append (NSPoint(x:400, y:600))
+points.append (NSPoint(x:0, y:600))
+points.append (NSPoint(x:0, y:200))
+canvas.drawCustomShape(with: points)
+
+canvas.fillColor = Color(hue: 47, saturation: 96, brightness: 99, alpha: 100)
+var points2:[NSPoint] = []
+points2.append (NSPoint(x:400, y:600))
+points2.append (NSPoint(x:0, y:200))
+points2.append (NSPoint(x:400, y:200))
+canvas.drawCustomShape(with: points2)
+
+canvas.fillColor = Color(hue: 16, saturation: 83, brightness: 100, alpha: 100)
+
+for y in stride(from: 600, to: 200, by: -44.44){
+    for x in stride(from: 0, to: 400, by: 44.44){
+        var points3:[NSPoint] = []
+        points3.append (NSPoint(x:x, y:y))
+        points3.append (NSPoint(x:x, y:y - 44.44))
+        points3.append (NSPoint(x:x + 44.44, y:y))
+        canvas.drawCustomShape(with: points3)
+    }
+}
+//: ## Template code
+//: The code below is necessary to see the result of your work in the Assistant Editor at right. Please do not remove.
 //: ## Template code
 //: The code below is necessary to see the result of your work in the Assistant Editor at right. Please do not remove.
 PlaygroundPage.current.liveView = canvas.imageView
