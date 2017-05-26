7e949ab0fa8ba996a7660c5677a15eecb0cc7f41
Kathleen McLaughlin <kathleen.mclaughlin.17@gmail.com>
2017-05-26 10:03:20 -0400

    lel

--- ExamScenarios/ExamScenarios.playground/Pages/Scenario 5.xcplaygroundpage/Contents.swift
+++ ExamScenarios/ExamScenarios.playground/Pages/Scenario 5.xcplaygroundpage/Contents.swift
@@ -24,6 +24,31 @@ import PlaygroundSupport
 let canvas = Canvas(width: 400, height: 600)
 
 // Your code below...
+canvas.fillColor = Color.black
+canvas.drawRectangle(centreX: 200, centreY: 300, width: 400, height: 600)
+canvas.defaultLineWidth = 15
+var z = 1
+for y in stride(from: 0, through: 400, by: 67){
+    for x in stride(from: 0, through: 400, by: 67){
+        
+        if z == 1 || z == 8 || z == 15 || z == 22 || z == 29 || z == 36{
+            canvas.lineColor = Color.white
+            
+        } else { canvas.lineColor = Color(hue: 290, saturation: 100, brightness: 100, alpha: 100)
+        }
+        
+        canvas.drawLine(fromX: x + 20, fromY: y + 20, toX: x + 45, toY: y + 45)
+        canvas.drawLine(fromX: x + 10, fromY: y + 10, toX: x + 10, toY: y + 35)
+        canvas.drawLine(fromX: x + 10, fromY: y + 10, toX: x + 35, toY: y + 10)
+        z += 1
+        
+    }
+}
+canvas.defaultLineWidth = 2
+canvas.lineColor = Color.white
+canvas.drawLine(fromX: 0, fromY: 450, toX: 600, toY: 450)
+canvas.textColor = Color.white
+canvas.drawText(message: "the velvet underground", size: 30, x: 0, y: 400)
 
 //: ## Template code
 //: The code below is necessary to see the result of your work in the Assistant Editor at right. Please do not remove.
